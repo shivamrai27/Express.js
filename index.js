4ec6f515c5ac46adf28831a994adcf621751dd37
shivamrai27 <83501750+shivamrai27@users.noreply.github.com>
2023-11-01 23:58:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.send('Hello Express!');
+}).listen(3000, () => {
+    console.log('Server is running');
+})

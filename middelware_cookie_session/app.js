200dd036264d913a18bf508de44669b4c2a5e645
shivamrai27 <200shivamrai@gmail.com>
2023-11-06 03:51:55 +0530

    ExpressJs

--- middelware_cookie_session/app.js
+++ middelware_cookie_session/app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var session = require('express-session');
 
 var app = express();
 
@@ -13,6 +13,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(session({
+  resave: false,
+  saveUnitialized: false,
+  secret: "first session"
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,12 +28,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- middelware_cookie_session/routes/index.js
+++ middelware_cookie_session/routes/index.js
@@ -3,32 +3,46 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.cookie("age", 25);
   res.render("index");
 });
-router.get('/set', function (req, res) {
-  res.cookie("age", "Shivam");
-  res.render("set");
-})
-router.get('/check', function (req, res) {
-  const cookie = req.cookies;
-  res.render("check")
-  console.log(cookie);
-})
+
 //Middleware created
-/*
-var a = 10;
-router.use(function (req, res, next) {
-  a = a + 1;
-  next();
+// var a = 10;
+// router.use(function (req, res, next) {
+//   a = a + 1;
+//   next();
+// })
+
+// router.get('/middleware', function (req, res, next) {
+//   res.send(`On profile ${a}`);
+// });
+//=================================================================
+
+// //Cookie created and checked
+// set cookie
+// router.get('/set', function (req, res) {
+//   res.cookie("age", "Shivam");
+//   res.render("set");
+// })
+// //requesting cookies
+// router.get('/check', function (req, res) {
+//   const cookie = req.cookies;
+//   res.render("check")
+//   console.log(cookie);
+// })
+//===================================================================
+
+//Session created
+router.get('/session', function (req, res, next) {
+  req.session.ban = true; //Session created with name of ban
+  res.send('session is created');
 })
-
-router.get('/middleware', function (req, res, next) {
-  res.send(`On profile ${a}`);
-});
-*/
-
-//Cookie 
-
-
+router.get('/check-session', function (req, res, next) {
+  // console.log(req.session);
+  if (req.session.ban === true) {
+    res.send("You are banned");
+  } else {
+    res.send('You are not banned');
+  }
+})
 module.exports = router;
